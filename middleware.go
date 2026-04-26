6578f1a84cc63b9d42ee4a76f7334ebf9d3a3892
LuK1337 <priv.luk@gmail.com>
2026-04-26 19:41:10 +0200

    Stream command output in foreground mode

--- middleware.go
+++ middleware.go
@@ -48,6 +48,11 @@ func (m Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddy
 		argv[index] = repl.ReplaceAll(argument, "")
 	}
 
+	if m.Foreground {
+		m.stdWriter = w
+		m.errWriter = w
+	}
+
 	err := m.run(argv)
 
 	if m.PassThru {
@@ -56,6 +61,10 @@ func (m Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddy
 		}
 
 		return next.ServeHTTP(w, r)
+	}
+
+	if m.Foreground {
+		return err
 	}
 
 	var resp struct {
